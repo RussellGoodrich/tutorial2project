4228678268e5370cfded841aab5f692c73811e74
Industrial Blog <theindustrialdev@gmail.com>
2021-03-09 23:16:32 -0500

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,22 +10,91 @@ public class PlayerScript : MonoBehaviour
     public float speed;
 
     public Text score;
+	public Text win;
+	public Text lives;
 
     private int scoreValue = 0;
+	private int livesValue = 3;
+	private bool facingRight = true;
+	
+	public AudioClip victory;
+	public AudioSource victorySource;
+	
+	private bool isOnGround;
+	public Transform groundcheck;
+	public float checkRadius;
+	public LayerMask allGround;
+	
+	Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+		lives.text = "Lives: " + livesValue.ToString();
+		anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+		
+		if (facingRight == false && hozMovement > 0)
+		{
+			Flip();
+		}
+		else if (facingRight == true && hozMovement < 0)
+		{
+			Flip();
+		}
+		
+		if (isOnGround == false)
+        {
+          anim.SetInteger("State", 2);
+        }
+		
+		if (isOnGround == true && hozMovement == 0)
+		{
+			anim.SetInteger("State", 0);
+		}
+		
+		isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+    }
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 Scaler = transform.localScale;
+		Scaler.x = Scaler.x * -1;
+		transform.localScale = Scaler;
+	}
+	
+	void Update()
+    {
+
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+          anim.SetInteger("State", 1);
+        }
+
+		if (Input.GetKeyUp(KeyCode.D))
+        {
+          anim.SetInteger("State", 0);
+        }
+
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+          anim.SetInteger("State", 1);
+        }
+
+		if (Input.GetKeyUp(KeyCode.A))
+        {
+			anim.SetInteger("State", 0);
+        }
 		
 		if (Input.GetKey("escape"))
 		{
@@ -38,20 +107,42 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
 			
-			if (scoreValue >= 2)
+			if (scoreValue == 4)
 			{
-				score.text = scoreValue.ToString() + " ; you win! Game developed by Russell Goodrich.";
+				livesValue = 3;
+				lives.text = "Lives: " + livesValue.ToString();
+				transform.position = new Vector3(60.0f, 0.0f, 0.0f); 
+			}
+			
+			if (scoreValue >= 8)
+			{
+				win.text = "You win! Game developed by Russell Goodrich.";
+				victorySource.clip = victory;
+				victorySource.Play();
 			}
         }
+		
+		if (collision.collider.tag == "Enemy")
+		{
+			livesValue -= 1;
+			lives.text = "Lives: " + livesValue.ToString();
+			Destroy(collision.collider.gameObject);
+			
+			if (livesValue <= 0)
+			{
+				win.text = "You lose! Game developed by Russell Goodrich.";
+				Destroy(gameObject);
+			}
+		}
 
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
